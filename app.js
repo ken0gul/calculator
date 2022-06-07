f38da48dbb62789c949dd1de289369c6982abcb4
ken0gul <ken0gul@gmail.com>
2022-06-07 15:21:25 -0400

    Add more funtionality

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let object = {};
 let currentInput;
 let result;
 let currentOperation;
+let displayValue = '';
 
 // Creating calculation functions
 // Add
@@ -18,17 +19,17 @@ function add(num1,num2) {
 
 // Subtract
 function subtract (num1,num2) {
-    return num1 - num2;
+    return (num1 - num2);
 }
 
 //Multiply
 function multiply(num1,num2) {
-    return num1 *num2;
+    return (num1 * num2);
 }
 
 // Divide 
 function divide(num1,num2) {
-    return num1/num2;
+    return (num1/num2);
 }
 
 
@@ -36,7 +37,7 @@ function divide(num1,num2) {
 function operate(operator, num1,num2) {
     console.log(operator, num1, num2)
     if(operator === 'add') return add(num1,num2);
-    if(operator === 'subtract') return subtract(num1,num2)
+    if(operator === 'minus') return subtract(num1,num2)
     if(operator === 'multiply') return multiply(num1,num2)
     if(operator === 'divide') return divide(num1,num2)
  }
@@ -45,41 +46,97 @@ function operate(operator, num1,num2) {
  function populate() {
     numbers.forEach(number => {
         number.addEventListener('click', e => {
-                if(array.length < 2) {
-                    
-                    currentInput = Number(e.target.dataset.id);
-                    
-                    display.textContent += currentInput;
-                }
+                
+            currentInput = e.target.dataset.id;
+            displayValue  += currentInput;
+            displayValue = +displayValue;
+            display.textContent = displayValue;
 
         })
     })
-    return currentInput;
+    return displayValue;
  }
 
  populate()
+ 
 
  // Creating operator buttons
  function calc() {
 
      button.forEach(button => {
          button.addEventListener('click', e=> {
-             currentOperation = e.target.dataset.id
-             
-             currentInput = Number(display.textContent)
-             display.textContent = "";
-             array.push(currentInput);
+              
+                
+
+
+
+            currentOperation = e.target.dataset.id;
+            let operator;
+                if(currentOperation === 'add') {
+                     operator = '+';
+                    }
+                if (currentOperation === 'minus') {
+                        operator = '-';
+                    }
+                    
+                if (currentOperation === 'multiply') {
+                        operator = '*';
+                    }
+                    
+                if (currentOperation === 'divide') {
+                        operator = '/';
+                    }
+                    display.textContent =  `${+displayValue} ${operator} `
+                    array.push(displayValue);
+                    // Reset display value to an empty string before moving on
+                    displayValue =  '';
+                
+                    
+
+                    
+
             })
         })
     }
 
     calc()
 
+
     // Equal operator
-    equal.addEventListener('click' , () =>  {
-        array.push(currentInput);
-        let [num1, num2] = array;
-        let result =operate(currentOperation, num1, num2)
-        display.textContent = result;
+    equal.addEventListener('click' , (e) =>  {
+        array.push(displayValue);
+
        
+        let [num1, num2, ...num] = array;
+
+        //More destructuring
+
+        let result =operate(currentOperation, num1, num2)
+        
+        console.log(result)
+        display.textContent = result;
+        displayValue = result;
+        console.log(`Array: ` + array)
+        
+        if(result) {
+            // Reset array 
+            array.length =0 ;   
+            let numbers = [];
+         for (let number of num ) {
+            numbers.push(number);
+            console.log(numbers)
+            let [num1, num2, ...num] = numbers.slice(-2);
+            result = operate(currentOperation, num1, num2);
+            console.log(result)
+            display.textContent = result;
+            // array.length = 0;
+            console.log('Array : ' + array);
+            
+            
+         }
+        }
+
+        
     })
+
+   
