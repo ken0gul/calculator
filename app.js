66d5e8543da11a4423bad22ff59f36e51e502786
ken0gul <ken0gul@gmail.com>
2022-06-13 17:23:18 -0400

    Add comments that explain important steps

--- app.js
+++ app.js
@@ -74,15 +74,23 @@ function operate(operator, num1,num2) {
 
  // Create a function for all calculations
  function calcAll (e) {
-    // When there is no input firstNumber is null and we do not have any result
+    // When there is no input firstNumber is null and we do not have any result of any calculation
+    
     if(firstNumber === null && !result) {
 
         currentOperation = e.target.dataset.operation;
         firstNumber = Number(display.textContent);
+        console.log("1- We set firstNumber and currentOperation for the first time here")
+        // Clear the display after every operation
         display.textContent = "";
         
+
         } else if (firstNumber !== null && !result ) {
+            // Here we have the firstNumber but no calculation made yet so we set secondNumber
+            // After that it takes firstNumber and secondNumber and make a calculation down there where we have firstNumber && secondNumber 
             secondNumber = Number(display.textContent)
+            // Then clear the display again
+            console.log("2- Set the secondNumber")
             display.textContent = ''
         } else if (result){
             currentOperation = e.target.dataset.operation;
@@ -90,15 +98,22 @@ function operate(operator, num1,num2) {
             display.textContent = "";
         
         }
-
+        
+        // After we set the secondNumber in the step 2, we make calculation based on these 2 numbers below
         if (firstNumber && secondNumber ) {
             if(!result && isNextOperationComplete === false) {
             newResult = operate(currentOperation, firstNumber, secondNumber);
+            console.log("3- Here we have newResult from the calculation of firstNumber and secondNumber we got in step 2")
+
+            // Then we set firstNumber to newResult so that we can use this result for the next operation as firstNumber.
             firstNumber = newResult;
+
+            // we set nextOperation because we cannot use currentOperation since it belongs to the previous one.
             nextOperation = e.target.dataset.operation;
+
+            // We had a calculation and an operation is made without using "=" key. so we set isNextOperationComplete to true which was false by default.
             isNextOperationComplete = true;
 
-            console.log(firstNumber, secondNumber, nextOperation)
             
         
            
@@ -106,10 +121,16 @@ function operate(operator, num1,num2) {
 
         else if(firstNumber === newResult) {
             currentOperation = e.target.dataset.operation;
-            console.log(currentOperation)
-           firstNumber =operate(nextOperation, firstNumber,secondNumber);
-           console.log(currentOperation)
-           isNextOperationComplete = false;
+            console.log('currentOperation : ', currentOperation);
+            console.log('nextOperation: ', nextOperation);
+            console.log('firstNumber: ', firstNumber);
+            // the else if in step 2 fires here because we set the secondNumber using display.textContent again
+            // And we already had set first number getting the newResult and they were equal so we use that firstNumber and secondNumber to get a new
+            // firstNumber and we got the secondNumber from step 2 so we have a new operation here and set the isNextOperationComplete to false because we set it to true
+            // in the previous operation
+            console.log('secondNumber: ', secondNumber);
+            firstNumber =operate(nextOperation, firstNumber,secondNumber);
+            isNextOperationComplete = false;
         }
 
         
